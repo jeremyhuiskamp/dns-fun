12c37180843db22808cea6054f3c0492cfda113f
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2025-04-29 23:52:28 +0200

    simplify name compression tests

--- name_compressor_test.go
+++ name_compressor_test.go
@@ -6,67 +6,54 @@ import (
 	"testing"
 )
 
+func TestLookupUnknownName(t *testing.T) {
+	nc := dns.NewNameCompressor()
+	tryCompress(t, nc, 20, name("foo", "com"), 0, "foo", "com")
+}
+
 func TestBasicNameCompression(t *testing.T) {
 	nc := dns.NewNameCompressor()
 	nc.Compress(5, name("foo", "com"))
-	prefix, offset := nc.Compress(20, name("foo", "com"))
-	if len(prefix) != 0 {
-		t.Errorf("expected no prefix, but got %q", prefix)
-	}
-	if offset != 5 {
-		t.Errorf("expected to find back offset 5, but got %d", offset)
-	}
+
+	tryCompress(t, nc, 20, name("foo", "com"), 5)
 }
 
 func TestReferenceLongerName(t *testing.T) {
 	nc := dns.NewNameCompressor()
 	nc.Compress(5, name("foo", "bar", "com"))
 
-	prefix, offset := nc.Compress(20, name("bar", "com"))
-	if offset != 9 { // 5 + len("foo") + 1
-		t.Errorf("expected offset 9, but got %d", offset)
-	}
-
-	if len(prefix) != 0 {
-		t.Errorf("expected to skip all prefixes, but got %q", prefix)
-	}
+	// 9 = 5 + len("foo") + 1
+	tryCompress(t, nc, 20, name("bar", "com"), 9)
 }
 
 func TestReferenceShorterName(t *testing.T) {
 	nc := dns.NewNameCompressor()
 	nc.Compress(5, name("bar", "com"))
 
-	prefix, offset := nc.Compress(20, name("foo", "bar", "com"))
-	if offset != 5 {
-		t.Errorf("expected offset 5, but got %d", offset)
-	}
-
-	if exp := name("foo"); !slices.Equal(prefix, exp) {
-		t.Errorf("expected prefix %q, but got %q", exp, prefix)
-	}
+	tryCompress(t, nc, 20, name("foo", "bar", "com"), 5, "foo")
 }
 
 func TestReferenceNameWithSharedParent(t *testing.T) {
 	nc := dns.NewNameCompressor()
 	nc.Compress(5, name("qux", "bar", "com"))
 
-	prefix, offset := nc.Compress(20, name("foo", "bar", "com"))
-	if offset != 9 { // start of "bar"
-		t.Errorf("expected offset 9, but got %d", offset)
-	}
-
-	if exp := name("foo"); !slices.Equal(prefix, exp) {
-		t.Errorf("expected prefix %q, but got %q", exp, prefix)
-	}
+	tryCompress(t, nc, 20, name("foo", "bar", "com"), 9, "foo")
 }
 
-func TestLookupUnknownName(t *testing.T) {
-	nc := dns.NewNameCompressor()
-	prefix, offset := nc.Compress(20, name("foo", "com"))
-	if offset != 0 {
-		t.Errorf("expected to find no offset, but got %d", offset)
+func tryCompress(
+	t *testing.T,
+	nc dns.NameCompressor,
+	offset uint16,
+	name dns.Name,
+	expPointer uint16,
+	expPrefix ...dns.Label,
+) {
+	t.Helper()
+	gotPrefix, gotPointer := nc.Compress(offset, name)
+	if gotPointer != expPointer {
+		t.Errorf("expected pointer %d but got %d", expPointer, gotPointer)
 	}
-	if exp := name("foo", "com"); !slices.Equal(exp, prefix) {
-		t.Errorf("expected prefix %q, but got %q", exp, prefix)
+	if !slices.Equal(gotPrefix, expPrefix) {
+		t.Errorf("expected prefix %q but got %q", expPrefix, gotPrefix)
 	}
 }
