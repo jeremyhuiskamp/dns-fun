8a92e6b8d4fe9e9c2671c2f23ced1bd7084be326
Jeremy Huiskamp <jeremy.huiskamp@gmail.com>
2025-05-01 15:35:55 +0200

    very rudimentary iterative resolver
    Just as a proof-of-concept, definitely not ready for real use.
    
    Can lookup:
    - straightforward things, like google.com
      - linear search, we always get additional records with ip addresses
        corresponding to authorities
    - trickier things, like google.de
      - de name servers know that the authoritative servers are
        ns?.google.com, but they don't know the ip addresses of .com hosts
    - CNAMEs like www.thoughtworks.com
    
    Obvious limitations:
    - no caching
    - generally ignores AAAA records, though I do seem to have a working v6
      address, so I should try that
    - starts with only one root server, v4 only
    - no notion of timeouts, retries, etc
    - UDP only
    
    Generally gets the same results as `host` does.  In some cases, the
    results differ, I'm guessing because I'm choosing arbitrary different NS
    records (eg, google.com zone has 4 authoritative NS).

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,14 +2,85 @@ package main
 
 import (
 	"dns"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"slices"
+	"strings"
 )
 
+var ripeRootIP = net.ParseIP("193.0.14.129")
+
 func main() {
+	for _, name := range os.Args[1:] {
+		fmt.Printf("resolving %q\n", name)
+		rsp, err := resolve(ripeRootIP, parseName(name))
+		if err != nil {
+			log.Printf("WARN: unable to query: %s", err)
+			continue
+		}
+
+		fmt.Printf("%d Answers:\n", len(rsp.Answers))
+		for _, answer := range rsp.Answers {
+			printResource(answer)
+		}
+
+		if len(rsp.Answers) < 1 { // fallback info, in case we didn't get an answer...
+			fmt.Printf("%d Authorities RRs:\n", len(rsp.Authorities))
+			for _, authority := range rsp.Authorities {
+				printResource(authority)
+			}
+			fmt.Printf("%d Additional RRs:\n", len(rsp.Additional))
+			for _, additional := range rsp.Additional {
+				printResource(additional)
+			}
+		}
+	}
+}
+
+func resolve(serverIP net.IP, host dns.Name) (dns.Message, error) {
+	rsp, err := query(serverIP, host)
+	if err != nil {
+		return dns.Message{}, err
+	}
+
+	answers := findAnswers(host, rsp)
+	for _, answer := range answers {
+		if answer.Type == dns.A {
+			return rsp, nil
+		}
+
+		if cname, ok := answer.Data.(dns.Name); ok && answer.Type == dns.CNAME {
+			return resolve(ripeRootIP, cname)
+		}
+	}
+
+	nextServerName, nextServerIP := findAnAuthoritativeServer(rsp)
+	if nextServerIP != nil {
+		return resolve(nextServerIP, host)
+	}
+
+	if nextServerName != nil {
+		rsp, err := resolve(ripeRootIP, nextServerName)
+		if err != nil {
+			return dns.Message{}, nil
+		}
+		answers := findAnswers(nextServerName, rsp)
+		for _, answer := range answers {
+			if ip, ok := answer.Data.(net.IP); ok && answer.Type == dns.A {
+				return resolve(ip, host)
+			}
+		}
+	}
+
+	return rsp, errors.New("could not find authoritative server")
+}
+
+func query(serverIP net.IP, host dns.Name) (dns.Message, error) {
 	question := dns.Question{
-		Name:  dns.Name{"google", "com"},
+		Name:  host,
 		Type:  dns.A,
 		Class: dns.IN,
 	}
@@ -22,49 +93,99 @@ func main() {
 
 	buf, err := query.WriteTo(nil)
 	if err != nil {
-		log.Fatalf("couldn't serialize query: %s\n", err)
+		return dns.Message{}, fmt.Errorf("couldn't serialize query: %s", err)
 	}
 
-	root := &net.UDPAddr{
-		IP:   net.ParseIP("198.41.0.4"),
+	serverAddr := &net.UDPAddr{
+		IP:   serverIP,
 		Port: 53,
 	}
 
-	conn, err := net.DialUDP("udp", nil, root)
+	conn, err := net.DialUDP("udp", nil, serverAddr)
 	if err != nil {
-		log.Fatalf("couldn't dial root server: %s\n", err)
+		return dns.Message{}, fmt.Errorf("couldn't dial root server: %s", err)
 	}
 
 	n, err := conn.Write(buf)
 	if err != nil {
-		log.Fatalf("unable to write udp message: %s\n", err)
+		return dns.Message{}, fmt.Errorf("unable to write udp message: %s", err)
 	} else if n < len(buf) {
-		log.Printf("WARN: wrote only %d bytes of %d byte message\n", n, len(buf))
+		return dns.Message{}, fmt.Errorf("wrote only %d bytes of %d byte message", n, len(buf))
 	}
 
 	rspBuf := make([]byte, 1024)
 	n, err = conn.Read(rspBuf)
 	if err != nil {
-		log.Fatalf("couldn't read udp message: %s\n", err)
+		return dns.Message{}, fmt.Errorf("couldn't read udp message: %s", err)
 	}
 
-	rsp, err := dns.ParseMessage(rspBuf[:n])
-	if err != nil {
-		log.Fatalf("couldn't parse response: %s\n", err)
-	}
+	return dns.ParseMessage(rspBuf[:n])
+}
 
-	fmt.Printf("%d Answers:\n", len(rsp.Answers))
+func parseName(hostname string) dns.Name {
+	parts := strings.Split(hostname, ".")
+	// well, I guess that's a disadvantage of having a Label type...
+	name := make([]dns.Label, len(parts))
+	for i, p := range parts {
+		name[i] = dns.Label(p)
+	}
+	return name
+}
+
+func findAnswers(name dns.Name, rsp dns.Message) []dns.Resource {
+	var answers []dns.Resource
 	for _, answer := range rsp.Answers {
-		printResource(answer)
+		// aside from malicious responses, is there a reason that we'd get
+		// non-matching answers in a response?
+		if slices.Equal(answer.Name, name) {
+			answers = append(answers, answer)
+		}
 	}
-	fmt.Printf("%d Authorities RRs:\n", len(rsp.Authorities))
+	return answers
+}
+
+func findAnAuthoritativeServer(rsp dns.Message) (dns.Name, net.IP) {
 	for _, authority := range rsp.Authorities {
-		printResource(authority)
+		if authority.Type != dns.NS {
+			continue
+		}
+
+		authorityName, ok := authority.Data.(dns.Name)
+		if !ok {
+			continue
+		}
+
+		// TODO: make sure that the authority is an authority for the domain
+		// or some parent zone of our actual target.
+		// But is there any reason, aside from malicious responses, that we'd
+		// get unrelated authorities here?
+
+		for _, additional := range rsp.Additional {
+			// TODO: support AAAA as well
+			// For now we probably need to prefer A at least, since I don't have
+			// a v6 address to test from!
+			if additional.Type != dns.A {
+				continue
+			}
+
+			if !slices.Equal(authorityName, additional.Name) {
+				continue
+			}
+
+			if ip, ok := additional.Data.(net.IP); ok {
+				fmt.Printf("authority: %s, %s, %s\n",
+					authority.Name, authorityName, ip)
+				return authorityName, ip
+			}
+		}
+		// Oh, we might get an authority with no ip address in it!
+		// Eg, the "de" authoritative servers know that ns1.google.com is
+		// an authority for google.de, but they don't know the ip address
+		// of ns1.google.com because it's in a different zone!
+		fmt.Printf("authority: %s, %s, ??\n", authority.Name, authorityName)
+		return authorityName, nil
 	}
-	fmt.Printf("%d Additional RRs:\n", len(rsp.Additional))
-	for _, additional := range rsp.Additional {
-		printResource(additional)
-	}
+	return nil, nil
 }
 
 func printResource(r dns.Resource) {
